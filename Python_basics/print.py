9e896c461ac8b740f112c2c594a5443de93d11dd
Vinicius Gabriel <gabriel02091995@gmail.com>
2021-12-07 18:35:18 -0300

    07/12/2021

---
+++ Python_basics/print.py
@@ -0,0 +1,10 @@
+
+print('Luiz', 'Otavio', sep='-') #falando para a função print utilizar o traço - como separador. Luiz-Otavio
+print('João', 'e', 'Maria', sep='-') #João-e-Maria
+
+
+print('Luiz', 'Otavio', sep='-', end='###') #o end aglutina o final dessa sentença com a próxima, com o caractere escolhido.
+print('João', 'e', 'Maria', sep='-')
+
+print('824', '176', '070', sep='-', end='.')
+print(18)
---
+++ Python_basics/strings.py
@@ -0,0 +1,1 @@
+#Cita que é normal ouvir que tudo(quase) em Python é um objeto.
