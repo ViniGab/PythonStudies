44762f7f49cfc8ddab35e724e92e72e6c07b9af2
Vinicius Gabriel <gabriel02091995@gmail.com>
2021-12-07 19:45:58 -0300

    07/12/2021

---
+++ Python_basics/primitivos.py
@@ -0,0 +1,23 @@
+"""
+Tipos de dados
+
+str - string
+int - inteiro
+float - real/ponto flutuante
+bool - booleano/lógico
+"""
+
+print(type('Vinicius'))  # <class 'str'>
+print(type(5.5))  # <class 'float'>
+print(type(123))  # <class 'int'>
+print(type(False))  # <class 'bool'>
+print(type(int('10')))  #para converter a string de número para int.
+
+name = input('name: ')
+
+print(f'Seu nome é {name}! Seja bem vindo!')
+
+
+
+
+
--- Python_basics/strings.py
+++ Python_basics/strings.py
@@ -1,1 +1,15 @@
 #Cita que é normal ouvir que tudo(quase) em Python é um objeto.
+
+"""
+str - string
+"""
+
+print('Aspas simples')
+print("Aspas duplas")
+print(123456)
+print("Para usar isso aqui 'aspas simples' dentro da string é só inverter")
+print('Para usar isso aqui "aspas simples" dentro da string é só inverter')
+print("Esse é meu \"texto\" (str).")  #Temos esse jeito feio também.
+print("para quebrar essa linha podemos usar isso \n aqui.")
+print(r'para nao quebrar essa linha e manter isso \n aqui usamos o "r" no início.')
+
