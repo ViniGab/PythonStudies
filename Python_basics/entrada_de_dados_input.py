d1f58ff0ed65d8027c0eeb3afad09ece4ff772aa
Vinicius Gabriel <gabriel02091995@gmail.com>
2021-12-16 17:00:27 -0300

    16/12/2021 pt 1

---
+++ Python_basics/entrada_de_dados_input.py
@@ -0,0 +1,20 @@
+nome = input("Qual o seu nome? ")
+idade = input("Qual a sua idade? ")
+
+ano_nascimento = 2021-int(idade)
+
+# print(f'O usuário digitou {nome} e o tipo da variável é '
+#       f'{type(nome)}')
+
+print(f'{nome} tem {idade} anos e nasceu em {ano_nascimento}.')
+
+  # Sempre retorna str, mesmo que seja digitado nome.
+
+
+numero_1 = input('Digite um número: ')
+numero_2 = input('Digite outro número: ')
+numero_2 = int(numero_2)
+numero_1 = int(numero_1)
+
+# print(int(numero_1) + int(numero_2))
+print(numero_1 + numero_2)
