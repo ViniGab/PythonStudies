046fcb27d49d4b332779c529ffbab606d4c06572
Vinicius Gabriel <gabriel02091995@gmail.com>
2021-12-16 19:34:34 -0300

    16/12/2021 pt 3

---
+++ Python_basics/operadores_relacionais_simbolos.py
@@ -0,0 +1,14 @@
+"""
+Operadores Relacionais - == > >= < <= !=
+"""
+
+print( 2 == '2' )  #False
+
+num_1 = 2
+num_2 = 2
+
+expressao = num_1 == num_2
+print(expressao)  # True
+
+  # sem mais anotações, porém os simbolos são auto explicativos.
+
