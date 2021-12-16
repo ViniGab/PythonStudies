0b58aeb42d7ab53cd0fcf1947154a7dff946aa1f
Vinicius Gabriel <gabriel02091995@gmail.com>
2021-12-16 18:31:15 -0300

    16/12/2021 pt 2

---
+++ Python_basics/IF_ELIF_ELSE_BOOLEANS.py
@@ -0,0 +1,15 @@
+if False:
+    print("Sempre foi verdadeiro.")  # A expressão desta linha é condicionada pela decisão na linha superior.
+
+    num_1 = 2
+    num_2 = 4
+
+    print(num_1 + num_2)
+elif False:
+    print("Now it's True xD")
+elif True:
+    print("Now it's more True than before xD")
+else:
+    print("Não é verdadeiro xD")
+
+
