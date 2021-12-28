cf13b70f3c4d54452fba0a2c1c11f6b632665b45
Vinicius Gabriel <gabriel02091995@gmail.com>
2021-12-28 20:08:52 -0300

    28/12/2021 pt 3

--- Python_basics/listas.py
+++ Python_basics/listas.py
@@ -16,3 +16,26 @@ l3 = l1 + l2
 print(l3)  # [1, 2, 3, 4, 5, 6]
 l3.append('B')  #append acrescenta valores.
 print(l3)  # [1, 2, 3, 4, 5, 6, 'B']
+l3.insert(2, 'Lagoa')  #com o insert escolhemos a posição onde a inclusão será realizada.
+print(l3)  # [1, 2, 'Lagoa', 3, 4, 5, 6, 'B']
+l3.pop()  # pop retira o último elemento ou o selecionado na lista(0,1,2,3...)
+print(l3)  # [1, 2, 'Lagoa', 3, 4, 5, 6]
+del(l3[3:5])  # deleta mais de um elemento da lista.
+print(l3)  # [1, 2, 'Lagoa', 5, 6]
+
+
+#Convertendo range em lista. No caso objetos iteráveis podem ser convertidos em lista.
+l4 = list(range(1, 10))
+print(l2)
+
+l4 = list(range(1,10))
+print(l4)
+
+for valor in l4:
+    print(valor)
+
+l5 = ['String', True, 10, -20.5]
+
+for elem in l2:
+    print(f'O tipo de elem é {type(elem)} e seu valor é {elem}')
+
