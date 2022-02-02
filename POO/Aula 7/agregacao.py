6b0a9fbe56445cd2d8d6a6e2d1cf9a414937befc
Vinicius Gabriel <gabriel02091995@gmail.com>
2022-02-01 22:59:36 -0300

    01/02/2022 pt 4

---
+++ POO/Aula 7/agregacao.py
@@ -0,0 +1,21 @@
+"""
+Na agregação uma classe utiliza outra classe,
+E essa classe precisa da outra classe para funcionar
+Diferente da associação.
+"""
+
+from classes_agregacao import CarrinhoDeCompras, Produto
+
+carrinho = CarrinhoDeCompras()
+
+p1 = Produto('Camiseta', 50)
+p2 = Produto('iPhone', 10000)
+p3 = Produto('Caneca', 15)
+
+carrinho.inserir_produto(p1)
+carrinho.inserir_produto(p1)
+carrinho.inserir_produto(p2)
+carrinho.inserir_produto(p3)
+
+carrinho.lista_produto()
+print(carrinho.soma_total())
