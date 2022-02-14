07a28ceebc1f7fffb15ff238e1ac21351b266127
Vinicius Gabriel <gabriel02091995@gmail.com>
2022-02-14 19:26:43 -0300

    14/02/2022 pt 1

---
+++ POO/Aula 12/polimorfismo.py
@@ -0,0 +1,27 @@
+"""
+Polimorfismo é o principio que permite que classes derivadas
+de uma superclasse tenham métodos iguais(de mesma assinatura), mas
+comportamentos diferentes
+
+Mesma assinatura = Mesma quantidade e tipo de parâmetros.
+"""
+
+from abc import ABC, abstractmethod
+
+class A(ABC):
+    @abstractmethod
+    def fala(self, msg): pass
+
+class B(A):
+    def fala(self, msg):
+        print(f'B está falando {msg}')
+
+class C(A):
+    def fala(self, msg):
+        print(f'C está falando {msg}')
+
+
+b = B()
+c = C()
+b.fala('Um assunto')
+c.fala('Outro assunto')
