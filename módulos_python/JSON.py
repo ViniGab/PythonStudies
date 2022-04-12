2d106fac8e5bd28baa14bbdf364868b0229ae91f
Vinicius Gabriel <68884985+ViniGab@users.noreply.github.com>
2022-04-12 04:35:09 -0300

    pt 2 12/04/2022

--- módulos_python/JSON.py
+++ módulos_python/JSON.py
@@ -6,3 +6,27 @@ muito leve e de fácil utilização. Muito utilizado por APIS
 from dados import *
 import json
 
+# Transformando a lista dos clientes_dicionario localizada em dados em Json.
+# O ident é para melhor vizualização dos dados.
+
+dados_json = json.dumps(clientes_dicionario, indent=4)
+print(dados_json)
+
+# Transformando um arquivo json em dicionário novamente
+
+print(clientes_json)
+dicionario = json.loads(clientes_json) # Feito
+
+for chave, valor in dicionario.items():
+    print(chave)
+    print(valor)
+
+# Criando o dicionário em arquivo com formato Json (clientes.json).
+
+with open('clientes.json', 'w') as arquivo:
+    json.dump(clientes_dicionario, arquivo, indent=4)
+
+with open('clientes.json', 'r') as arquivo:
+    dados = json.load(arquivo)
+
+print(dados)
